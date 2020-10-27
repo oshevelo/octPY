d0a78c407aac74ddec898d3245fbb229a6858f3b
Altair <kolesov0703@gmail.com>
2020-10-28 01:55:07 +0200

    Homework to 28.10

--- mysite/settings.py
+++ mysite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'polls.apps.PollsConfig',
+    'jet',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- mysite/urls.py
+++ mysite/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
+    path(r'^jet/', include('jet.urls', 'jet')),
     path('admin/', admin.site.urls),
 ]
