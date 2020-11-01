7a20709d9ecfd5fb075ad397bad349c79c6059c4
Alex shevelo <oshevelo@softserveinc.com>
2020-11-01 14:19:49 +0200

    demo git flow

--- mysite_DRF/manage.py
+++ mysite_DRF/manage.py
@@ -2,7 +2,9 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-   
+
+
+
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
