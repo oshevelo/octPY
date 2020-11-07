38c2120b52dd988e93c419d4453b1426c53f3983
mauriceo22 <dellchester1@gmail.com>
2020-11-07 21:15:47 +0200

    User profile model create

--- gastronom/user_profile/models.py
+++ gastronom/user_profile/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    """Customer's profile"""
+
+    GENDER_CHOICES = (
+        (0, 'male'),
+        (1, 'female'),
+        (2, 'not specified'),
+    )
+
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    phone_number = models.CharField(max_length=12)
+    email = models.CharField(max_length=30)
+    birth_date = models.DateField(max_length=8)
+    gender = models.IntegerField(choices=GENDER_CHOICES, default=2)
