f38e458d6741c5fbe43ed6efc1be4ae25df2c0f2
Artem Kaika <factotum23@gmail.com>
2020-11-07 18:54:15 +0200

    Catalog model created

--- gastronom/catalog/models.py
+++ gastronom/catalog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Subcategory(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    category = models.ForeignKey(Category.name, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
