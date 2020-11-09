7e2eae766e2efc365472ce4b9fa7e84835b45894
Artem Kaika <factotum23@gmail.com>
2020-11-10 00:13:15 +0200

    test

--- gastronom/catalog/models.py
+++ gastronom/catalog/models.py
@@ -1,18 +1,18 @@
-from django.db import models
+# from django.db import models
+#
+#
+# class Category(models.Model):
+#     name = models.CharField(max_length=50)
+#     description = models.TextField(blank=True)
+#
+#     def __str__(self):
+#         return self.name
 
-
-class Category(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.TextField(blank=True)
-
-    def __str__(self):
-        return self.name
-
-
-class Subcategory(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.TextField(blank=True)
-    category = models.ForeignKey(Category.name, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
+#
+# class Subcategory(models.Model):
+#     name = models.CharField(max_length=200)
+#     description = models.TextField(blank=True)
+#     category = models.ForeignKey(Category.name, on_delete=models.CASCADE)
+#
+#     def __str__(self):
+#         return self.name
