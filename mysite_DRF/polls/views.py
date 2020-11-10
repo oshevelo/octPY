7f3faebbc8757ccc29142a6a9d4c7a3fa4a1fdb7
Anton0304 <kelaandkel34@gmail.com>
2020-11-10 23:02:48 +0200

    instalpsgr

--- mysite_DRF/polls/views.py
+++ mysite_DRF/polls/views.py
@@ -1,27 +1,19 @@
 from django.http import HttpResponse
-from django.http import Http404
-from polls.models import Question, Choice
+from .models import Question, Choice
 
 from rest_framework import generics
-from polls.serializers import QuestionSerializer, ChoiceSerializer
+from .serializers import QuestionSerializer
+from .serializers import ChoiceSerializer
 from django.shortcuts import get_object_or_404
-from django.urls import reverse
 
 
 def index(request):
     question_list = Question.objects.all()
-    return HttpResponse('http://127.0.0.1:8000' + reverse('list'))
+    return HttpResponse(', '.join([q.question_text for q in question_list]))
 
 
 def index_other(request):
     return HttpResponse("Hello, OTHER world. You're at the polls index.")
-
-
-class ChoiceList(generics.ListCreateAPIView):
-    serializer_class = ChoiceSerializer
-
-    def get_queryset(self):        
-        return Choice.objects.filter(question_id=self.kwargs.get('question_id')) 
 
 
 class QuestionList(generics.ListCreateAPIView):
@@ -29,11 +21,20 @@ class QuestionList(generics.ListCreateAPIView):
     serializer_class = QuestionSerializer
 
 
-
 class QuestionDetail(generics.RetrieveUpdateDestroyAPIView):
-    
     serializer_class = QuestionSerializer
 
     def get_object(self):
         return get_object_or_404(Question, pk=self.kwargs.get('question_id'))
 
+
+class ChoiceList(generics.ListCreateAPIView):
+    queryset = Choice.objects.all()
+    serializer_class = ChoiceSerializer
+
+
+class ChoiceDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ChoiceSerializer
+
+    def get_object(self):
+        return get_object_or_404(Choice, pk=self.kwargs.get('choice_id'))
