3efc2ff9f244f48df5fdc732c5f12c01f75ccd63
roman <shmuklerr@gmail.com>
2020-11-10 22:11:58 +0200

    add cart models

--- gastronom/cart/models.py
+++ gastronom/cart/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# from gastronom.product.models import Product
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, verbose_name='Owner')
+    creation_date = models.DateTimeField('Creation date', auto_now_add=True)
+
+    def __str__(self):
+        return f'User = {self.user}; pk = {self.pk}'
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, verbose_name='Cart')
+    # product = models.ForeignKey(Product, verbose_name='Product')
+    quantity = models.PositiveIntegerField('Quantity', default=1, null=True, blank=True)
+    price = models.DecimalField('Price', max_digits=15, decimal_places=2)
+    creation_date = models.DateTimeField('Creation date', auto_now_add=True)
--- gastronom/gastronom/settings.py
+++ gastronom/gastronom/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'cart',
 ]
 
 MIDDLEWARE = [
@@ -72,14 +74,7 @@ WSGI_APPLICATION = 'gastronom.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-'''
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
