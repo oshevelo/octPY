078cf9df80a5695fa086dbf6514d03891bdf1004
mauriceo22 <dellchester1@gmail.com>
2020-11-11 03:24:46 +0200

    models change

--- gastronom/user_profile/models.py
+++ gastronom/user_profile/models.py
@@ -4,16 +4,20 @@ from django.db import models
 
 class UserProfile(models.Model):
     """Customer's profile"""
+    
+    Male = 'Male'
+    Female = 'Female'
+    Not_specified = 'not specified'
 
     GENDER_CHOICES = (
-        (0, 'male'),
-        (1, 'female'),
-        (2, 'not specified'),
+        (Male, 'male'),
+        (Female, 'female'),
+        (Not_specified, 'not specified'),
     )
 
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=20)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
     phone_number = models.CharField(max_length=12)
-    email = models.CharField(max_length=30)
-    birth_date = models.DateField(max_length=8)
-    gender = models.IntegerField(choices=GENDER_CHOICES, default=2)
+    email = models.EmailField(max_length=30)
+    birth_date = models.DateField(null=True)
+    gender = models.IntegerField(choices=GENDER_CHOICES, default=Not_specified)
