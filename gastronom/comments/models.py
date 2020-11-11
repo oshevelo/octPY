3250a5a0a25ec7ca10fb87125ab227204eff397c
OleksandrPichkurov <alexandr.pichkurov@gmail.com>
2020-11-11 17:33:08 +0200

    models v1.1

--- gastronom/comments/models.py
+++ gastronom/comments/models.py
@@ -1,37 +1,29 @@
 from django.db import models
-from product.model import Product
+from django.contrib.auth.models import User
+# from product.model import Product
 # Create your models here.
 
 
 class Reviews(models.Model):
-    email = models.EmailField()
-    name = models.CharField(max_length=50)
-    review_photo = models.ImageField(upload_to='reviews/')
-    text = models.TextField(max_length=500)
-    product = models.ForeignKey(Product, verbose_name='Product', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='review_user')
+    text = models.TextField(max_length=5000)
+    # product = models.ForeignKey(Product, verbose_name='rewiew_product', on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     useful = models.SmallIntegerField(default=0)
+    reply_to = models.ForeignKey('self', on_delete=models.SET_NULL, verbose_name='reply_review', null=True, blank=True)
 
     class Meta:
         ordering = ('created',)
 
     def __str__(self):
-        return f'Review by {self.name} on {self.product}'
+        return f'Review by {self.user} on {self.product}'
 
 
-class NestedReviews(models.Model):
-    nested_review = models.ForeignKey(Reviews, verbose_name='Reviews on review', on_delete=models.SET_NULL, blank=True, null=True)
-    email = models.EmailField()
-    name = models.CharField(max_length=50)
-    review_photo = models.ImageField(upload_to='reviews/')
-    text = models.TextField(max_length=500)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    useful = models.SmallIntegerField(default=0)
+class CommentGalleryItem(models.Model):
+    review_photo = models.ImageField(upload_to='reviews/', null=True, blank=True)
+    review = models.ForeignKey(Reviews, on_delete=models.CASCADE)
 
-    class Meta:
-        ordering = ('created',)
 
-    def __str__(self):
-        return f'Review by {self.name} on {self.nested_review}'
+class UsefulPlusMinas(models.Model):
+    pass
