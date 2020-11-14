e00306d20b8649ed2f9780b26fac90f73a242c30
oksana <o.ieroshenko@gmail.com>
2020-11-14 21:20:46 +0200

    added views and serializers, fixed User

--- gastronom/notifications/models.py
+++ gastronom/notifications/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from gastronom.settings import INSTALLED_APPS
-
+from django.conf import settings
 
 
 class Notification(models.Model):
@@ -9,7 +9,7 @@ class Notification(models.Model):
     SOURCE = [(x, x) for x in INSTALLED_APPS]
     source = models.CharField(max_length=200, choices=SOURCE)
 
-    recipient = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+    recipient = models.ForeignKey(User, null=True, on_delete=models.CASCADE, related_name='notifications')
 
     send_methods = (
         ('email', 'E-mail'),
@@ -18,21 +18,21 @@ class Notification(models.Model):
     )
     send_method = models.CharField(choices=send_methods, default='email', max_length=20)
     timestamp = models.DateTimeField(auto_now_add=True)
-    verb = models.CharField(max_length=200)
+    message = models.TextField(max_length=200, default='message')
 
     class Meta:
         ordering = ("-timestamp",)
 
     def __str__(self):
-        return f"{self.source} {self.recipient} {self.verb} {self.timestamp} {self.send_method}"
+        return f"{self.source} {self.recipient} {self.message} {self.timestamp} {self.send_method}"
 
     @classmethod
-    def create_notifications(cls, source, recipient, verb, send_method):
+    def create_notifications(cls, source, recipient, message, send_method):
         """
         Create and save notification objects to database
         :param source: str, name of app making notification
         :param recipient: class User object or list of class User objects
-        :param verb: str, body of notification message
+        :param message: str, body of notification message
         :param send_method: str, name of send method
         """
         if isinstance(recipient, list):
@@ -40,14 +40,14 @@ class Notification(models.Model):
                 Notification.objects.create(
                     source=source,
                     recipient=user,
-                    verb=verb,
+                    message=message,
                     send_method=send_method,
-                    ).save()
+                    )
 
         else:
             Notification.objects.create(
                 source=source,
                 recipient=recipient,
-                verb=verb,
+                message=message,
                 send_method=send_method,
-            ).save()
+            )
--- gastronom/notifications/urls.py
+++ gastronom/notifications/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
-from . import views
+from notifications import views
+
 
 urlpatterns = [
-    path('', views.index, name='index'),
-
+    path('last/', views.NotificationListCreate().as_view(), name='last_created_notifications'),
+    path('recipient/<int:recipient_id>/', views.NotificationsByRecipient().as_view(),
+         name='notifications_by_recipient'),
+    path('recipient/<int:recipient_id>/nested/', views.QuestionChoiceNested().as_view(),
+         name='notifications_by_recipient_nested'),
 ]
