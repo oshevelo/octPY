fe8a6bef1643ae2e5f1dcf1b7f58622aa543466e
Artem Kaika <factotum23@gmail.com>
2020-11-16 14:58:38 +0200

    CatalogDetailed added

---
+++ gastronom/catalog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('catalog/', views.CatalogList.as_view(), name='list'),
+    path('catalog/<int:catalog_id>/', views.CatalogDetail.as_view(), name='details'),
+]
--- gastronom/catalog/views.py
+++ gastronom/catalog/views.py
@@ -3,7 +3,7 @@ from django.http import Http404
 from catalog.models import Catalog
 
 from rest_framework import generics
-from catalog.serializers import CatalogSerializer
+from catalog.serializers import CatalogSerializer, CatalogDetailedSerializer
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render
 
@@ -11,5 +11,12 @@ from django.shortcuts import render
 class CatalogList(generics.ListCreateAPIView):
     queryset = Catalog.objects.all()
     serializer_class = CatalogSerializer
+
+
+class CatalogDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = CatalogDetailedSerializer
+
+    def get_object(self):
+        return get_object_or_404(Catalog, pk=self.kwargs.get('catalog_id'))
 
 
