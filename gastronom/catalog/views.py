362243aa6275575be786705a65669d49bac39eda
Artem Kaika <factotum23@gmail.com>
2020-11-16 14:28:33 +0200

    admin Catalog registered, ViewList, Serializer

--- gastronom/catalog/views.py
+++ gastronom/catalog/views.py
@@ -1,3 +1,15 @@
+from django.http import HttpResponse
+from django.http import Http404
+from catalog.models import Catalog
+
+from rest_framework import generics
+from catalog.serializers import CatalogSerializer
+from django.shortcuts import get_object_or_404
 from django.shortcuts import render
 
-# Create your views here.
+
+class CatalogList(generics.ListCreateAPIView):
+    queryset = Catalog.objects.all()
+    serializer_class = CatalogSerializer
+
+
