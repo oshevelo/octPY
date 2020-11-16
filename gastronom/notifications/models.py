c05c5873407b4c2b242bed87758a3d55d3b66284
oksana <o.ieroshenko@gmail.com>
2020-11-16 16:37:38 +0200

    added send_email function

--- gastronom/notifications/models.py
+++ gastronom/notifications/models.py
@@ -1,16 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 from gastronom.settings import INSTALLED_APPS
-from django.conf import settings
+from notifications.sender import send_email
 
 
 class Notification(models.Model):
-
     SOURCE = [(x, x) for x in INSTALLED_APPS]
     source = models.CharField(max_length=200, choices=SOURCE)
-
     recipient = models.ForeignKey(User, null=True, on_delete=models.CASCADE, related_name='notifications')
-
     send_methods = (
         ('email', 'E-mail'),
         ('messenger', 'Messenger'),
@@ -18,7 +15,7 @@ class Notification(models.Model):
     )
     send_method = models.CharField(choices=send_methods, default='email', max_length=20)
     timestamp = models.DateTimeField(auto_now_add=True)
-    message = models.TextField(max_length=200, default='message')
+    message = models.TextField(max_length=200)
 
     class Meta:
         ordering = ("-timestamp",)
@@ -35,6 +32,9 @@ class Notification(models.Model):
         :param message: str, body of notification message
         :param send_method: str, name of send method
         """
+        if send_method == 'email':
+            send_func = send_email
+
         if isinstance(recipient, list):
             for user in recipient:
                 Notification.objects.create(
@@ -43,7 +43,7 @@ class Notification(models.Model):
                     message=message,
                     send_method=send_method,
                     )
-
+                send_func(recipient_email=user.email, message=message)
         else:
             Notification.objects.create(
                 source=source,
@@ -51,3 +51,8 @@ class Notification(models.Model):
                 message=message,
                 send_method=send_method,
             )
+            send_func(recipient_email=str(recipient.email), message=message)
+
+
+# Notification.create_notifications('notifications', recipient=[User.objects.get(id=1), User.objects.get(id=4)],
+# message='Subject: Hi there \n\n This is my first e-mail notification from Django.gastronom', send_method='email')
