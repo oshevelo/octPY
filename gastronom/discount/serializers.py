635deb686a6c24cd7292db803d9b6a2da255a339
Anton0304 <kelaandkel34@gmail.com>
2020-11-22 19:16:41 +0200

    Discount-Loyalty_1

--- gastronom/discount/serializers.py
+++ gastronom/discount/serializers.py
@@ -13,3 +13,9 @@ class DiscountPostSerializer(serializers.Serializer):
     cart = serializers.IntegerField()
     code = serializers.CharField(max_length=200)
     amount_cart = serializers.DecimalField(max_digits=10, decimal_places=4)
+    
+    
+    
+    
+    
+ .....
