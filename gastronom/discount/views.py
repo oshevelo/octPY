1ffbd3eb30bcc68a23596cfcda6a094c0d66518a
Anton0304 <kelaandkel34@gmail.com>
2020-11-22 19:07:58 +0200

    Discount(loyalty)

---
+++ gastronom/discount/views.py
@@ -0,0 +1,34 @@
+from rest_framework.authentication import TokenAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from .DiscountRequestAdapter import *
+from .application.DiscountService import DiscountService
+from .serializers import DiscountPostSerializer
+
+
+class Discounts(APIView):
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (TokenAuthentication, BasicAuthentication)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.discount_request_adapter = DiscountRequestAdapter()
+        self.discount = DiscountService()
+
+    def post(self, request) -> Response:
+        discount_data = DiscountPostSerializer(data=request.data)
+
+        if not discount_data.is_valid():
+            return Response({"message": "Not Valid"}, status=status.HTTP_400_BAD_REQUEST)
+
+        code, cart_number, cart_amount = self.discount_request_adapter.adapt_serializer_data(discount_data)
+
+        return self.discount.response_client_service(
+            code,
+            cart_number,
+            cart_amount
+        )
