60f9b4ff54f967fc572923c7cd25747fa56ef033
OleksandrPichkurov <alexandr.pichkurov@gmail.com>
2020-12-01 22:45:53 +0200

    add method for resize image

--- gastronom/comments/models.py
+++ gastronom/comments/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from gastronom.settings import REVIEW_IMAGE_SIZE
 from product.models import Product
 from PIL import Image
 # Create your models here.
+
+
+def review_photo_path(instance, filename):
+    review_photo_name = f'review_{instance.review.id}/{instance.review_photo}'  # make a folder for images in reviews
+    return review_photo_name
 
 
 class Review(models.Model):
@@ -21,13 +27,22 @@ class Review(models.Model):
 
 
 class ReviewImage(models.Model):
-    review_photo = models.ImageField(upload_to='reviews/', null=True, blank=True)
+    review_photo = models.ImageField(upload_to=review_photo_path, null=True, blank=True)
     review = models.ForeignKey(Review, on_delete=models.CASCADE)
-
 
     def __str__(self):
         return f'Image for {self.review}'
 
+    def save(self, *args, **kwargs): 
+        '''
+        Saving the picture in a scale for easy display in reviews 300x300
+        '''
+        super().save(*args, **kwargs)
+
+        if self.review_photo:
+            img = Image.open(self.review_photo.path)
+            img.thumbnail(REVIEW_IMAGE_SIZE, Image.LANCZOS)
+            img.save(self.review_photo.path)
 
 
 class ReviewRating(models.Model):
