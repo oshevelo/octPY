6ba5f0782f0046f298ad0007ab3c44e72d0343a6
tv <volianskyitv@gmail.com>
2020-12-13 18:40:29 +0200

    Add models of payment

--- gastronom/payment/models.py
+++ gastronom/payment/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-#
-# Create your models here.
+
+class Payment(models.Model):
+    payment_id = models.CharField('payment_id', max_length=50, primary_key=True, null=False)
+    order_id = models.CharField('order_id', max_length=50, primary_key=False, null=False)
+    payment_system = models.CharField('payment_system', max_length=100, primary_key=False, null=False)
+    total_amount = models.IntegerField('total_amount', primary_key=False, null=False)
+
+def __str__(self):
+    return self.payment_id
