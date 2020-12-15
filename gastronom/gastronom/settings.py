5023005b0b150720252361c6ba87d9a1a30bfd9d
OleksandrPichkurov <alexandr.pichkurov@gmail.com>
2020-12-15 14:02:47 +0200

    added permission class

--- gastronom/gastronom/settings.py
+++ gastronom/gastronom/settings.py
@@ -77,6 +77,7 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'gastronom.urls'
 
 REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticated',],
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 50
