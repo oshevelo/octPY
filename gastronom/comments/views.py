7987ccdf5d9ae6af9570819ab66748cb6991c705
OleksandrPichkurov <alexandr.pichkurov@gmail.com>
2020-12-22 18:52:31 +0200

    fix and added new permissions

--- gastronom/comments/views.py
+++ gastronom/comments/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import get_object_or_404
 
 from rest_framework import generics
 from rest_framework.pagination import LimitOffsetPagination
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework.permissions import IsAuthenticated
+from .permissions import IsOwnerOrReadOnly
 
 from .models import Review, ReviewImage, ReviewRating
 from notifications.views import create_notifications
@@ -13,23 +14,22 @@ from comments.serializers import ReviewSerializer, ReviewImageSerializer, Review
 
 class ReviewListCreate(generics.ListCreateAPIView):
     pagination_class = LimitOffsetPagination
-    permission_class = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAuthenticated]
     queryset = Review.objects.filter(reply_to=None).order_by('-created')
     serializer_class = ReviewSerializer
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
         reply = serializer.data['reply_to']
-    
+
         if reply:
             review_obj = Review.objects.get(id=reply)
             create_notifications(source='comments.apps.CommentsConfig', recipients=[review_obj.user], send_method='email', subject='Gastronom review', message='You have answer on your review')
 
 
-
 class ReviewRetrieve(generics.RetrieveUpdateAPIView):
     serializer_class = ReviewSerializer
-    permission_class = [IsAuthenticated]
+    permission_classes = [IsOwnerOrReadOnly]
 
     def get_object(self):
         return get_object_or_404(Review, pk=self.kwargs.get('review_id'))
@@ -37,20 +37,21 @@ class ReviewRetrieve(generics.RetrieveUpdateAPIView):
 
 class ReviewImageListCreate(generics.ListCreateAPIView):
     pagination_class = LimitOffsetPagination
+    permission_classes = [IsAuthenticated]
     queryset = ReviewImage.objects.all()
     serializer_class = ReviewImageSerializer
 
 
 class ReviewImageRetrieve(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = ReviewImageSerializer
-    permission_class = [IsAuthenticated]
+    permission_classes = [IsOwnerOrReadOnly]
 
     def get_object(self):
         return get_object_or_404(ReviewImage, pk=self.kwargs.get('review_id'))
 
 
 class ReviewRatingListCreate(generics.ListCreateAPIView):
-    permission_class = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
     pagination_class = LimitOffsetPagination
     queryset = ReviewRating.objects.all()
     serializer_class = ReviewRatingSerializer
