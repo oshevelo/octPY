dfafc049233a6de2d99ab4bb4f6e459736b54641
OleksandrPichkurov <alexandr.pichkurov@gmail.com>
2020-12-23 13:48:57 +0200

    add test on post method in ListCreate view

--- gastronom/comments/tests.py
+++ gastronom/comments/tests.py
@@ -15,11 +15,33 @@ class ReviewTestApi(TestCase):
 
     def setUp(self):
         self.client = APIClient()
-        self.user = User.objects.create_user(username='repka', email='', password='life230493')
+        self.user = User.objects.create_user(username='repka', email='',
+                                                password='life230493')
         self.client.login(username='repka', password='life230493')
         self.review_1 = Review.objects.create(user=self.user, text='some text')
 
-    def test_get_review_list(self):
+    def test_get_review_list_with_permission(self):
         response = self.client.get('/comments/reviews/')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json(), {'count': 1, 'next': None, 'previous': None, 'results': [{'user': {'username': 'repka'}, 'product': None, 'text': 'some text', 'created': response.data['results'][0]['created'], 'child': [], 'reply_to': None}]})
+        self.assertEqual(response.json(), {'count': 1, 'next': None,
+                                            'previous': None,
+                                            'results': [{
+                                                'user': {'username': 'repka'},
+                                                'product': None,
+                                                'text': 'some text',
+                                                'created': response.data['results'][0]['created'],
+                                                'child': [],
+                                                'reply_to': None
+                                                }]})
+
+    def test_post_review_list_with_permission(self):
+        response = self.client.post('/comments/reviews/', {'product': '',
+                                                            'text': 'some text',
+                                                            'child': []})
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.json(), {'child': [],
+                                           'created': response.data['created'],
+                                           'product': None,
+                                           'reply_to': None,
+                                           'text': 'some text',
+                                           'user': {'username': 'repka'}})
