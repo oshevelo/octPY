17a3f92805190b00285752a5ca81c4f55c73fe91
Artem Kaika <factotum23@gmail.com>
2020-12-26 08:25:25 +0200

    local fixes

--- gastronom/catalog/views.py
+++ gastronom/catalog/views.py
@@ -4,6 +4,7 @@ from catalog.models import Catalog
 
 from rest_framework import generics
 from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.permissions import IsAuthenticated
 from catalog.serializers import CatalogSerializer, CatalogDetailedSerializer, CatalogTreeSr
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render
@@ -13,6 +14,7 @@ class CatalogList(generics.ListCreateAPIView):
     queryset = Catalog.objects.all()
     serializer_class = CatalogSerializer
     pagination_class = LimitOffsetPagination
+    permission_classes = [IsAuthenticated]
 
 
 class CatalogDetail(generics.RetrieveUpdateDestroyAPIView):
@@ -25,6 +27,8 @@ class CatalogDetail(generics.RetrieveUpdateDestroyAPIView):
 class CatalogTree(generics.ListAPIView):
     serializer_class = CatalogTreeSr
     pagination_class = LimitOffsetPagination
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         return Catalog.objects.exclude(parent__isnull=False)
+
