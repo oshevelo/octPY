7236392b34496b02140c9ac20eb35cf8f0f730a7
tv <volianskyitv@gmail.com>
2021-02-02 11:37:08 +0200

    Update payment models

--- gastronom/payment/models.py
+++ gastronom/payment/models.py
@@ -1,15 +1,14 @@
 from django.db import models
 
 
-class Payment(models.Model): payment_id = models.CharField('payment_id', max_length=50, primary_key=True, null=False)
-
-
-order_id = models.CharField('order_id', max_length=50, primary_key=False, null=False)
-payment_system = models.CharField('payment_system', max_length=100, primary_key=False, null=False)
-payment_amount = models.IntegerField('payment_amount', primary_key=False, null=False)
-user_id = models.CharField('user_id', max_length=50, primary_key=False, null=False)
-payment_date = models.DateTimeField('payment_date', primary_key=False, null=False)
-paymetn_status = models.CharField('paymetn_status', max_length=50, primary_key=False, null=False)
+class Payment(models.Model):
+    payment_id = models.CharField('payment_id', max_length=50, primary_key=True, null=False)
+    order_id = models.CharField('order_id', max_length=50, primary_key=False, null=False)
+    payment_system = models.CharField('payment_system', max_length=100, primary_key=False, null=False)
+    payment_amount = models.IntegerField('payment_amount', primary_key=False, null=False)
+    user_id = models.CharField('user_id', max_length=50, primary_key=False, null=False)
+    payment_date = models.DateTimeField('payment_date', primary_key=False, null=False)
+    paymetn_status = models.CharField('paymetn_status', max_length=50, primary_key=False, null=False)
 
 
 def __str__(self):
