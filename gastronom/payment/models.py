4ae7bb19f034b1f2193b8ffa6629220cbbe6370b
tv <volianskyitv@gmail.com>
2021-02-08 16:39:28 +0200

    update models.py

--- gastronom/payment/models.py
+++ gastronom/payment/models.py
@@ -1,26 +1,109 @@
 from django.db import models
+from django.contrib.postgres.fields import JSONField
+from django_fsm import FSMField, transition
 
 
-class Payment(models.Model):
-    payment_id = models.CharField('payment_id', max_length=50, primary_key=True, null=False)
-    order_id = models.CharField('order_id', max_length=50, primary_key=False, null=False)
-    payment_system = models.CharField('payment_system', max_length=100, primary_key=False, null=False)
-    payment_amount = models.IntegerField('payment_amount', primary_key=False, null=False)
-    user_id = models.CharField('user_id', max_length=50, primary_key=False, null=False)
-    payment_date = models.DateTimeField('payment_date', primary_key=False, null=False)
-    paymetn_status = models.CharField('paymetn_status', max_length=50, primary_key=False, null=False)
+class PaymentSystem:
+    portmone = 'portmone'
+    cash = 'cash'
 
 
-def __str__(self):
-    return self.title
-
-    # Change name of table
-
-
-class Meta:
-    verbose_name = 'Specific task'
-    verbose_name_plural = 'All tasks'
+paymentsystem_list = [
+    (PaymentSystem.portmone, 'portmone'),
+    (PaymentSystem.cash, 'cash'),
+]
 
 
-def Payments():
-    return None
+class Status:
+    submitted = 'submitted'
+    processing = 'processing'
+    completed = 'completed'
+    suspended = 'suspended'
+    declined = 'declined'
+
+
+status_list = [
+    (Status.submitted, 'submitted'),
+    (Status.processing, 'processing'),
+    (Status.completed, 'completed'),
+    (Status.suspended, 'suspended'),
+    (Status.declined, 'declined'),
+]
+
+
+class Payments(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.PROTECT,
+                             null=True, blank=False
+                             )
+    order = models.OneToOneField('orders.Order', on_delete=models.PROTECT,
+                                 null=True, blank=True
+                                 )
+    paymentsystem = models.CharField(max_length=10,
+                                     choices=paymentsystem_list, default='portmone'
+                                     )
+    billAmount = models.FloatField(default=0.0)
+    payment_date = models.DateTimeField(auto_now_add=True)
+    status = FSMField(default=Status.submitted, choices=status_list)
+
+    def permissions(self, user):
+        if self.user == user:
+            if self.status == Status.submitted:
+                return ['can_create', 'can_edit', 'can_delete']
+            else:
+                return ['can_create']
+        else:
+            return []
+
+    @transition(field=status, source=['submitted', 'suspended'],
+                target='processing'
+                )
+    def submit(self):
+        pass
+
+    @transition(field=status, source='processing', target='suspended')
+    def suspend(self):
+        pass
+
+    @transition(field=status, source='processing', target='completed')
+    def complete(self):
+        pass
+
+    @transition(field=status, source=['processing', 'suspended'],
+                target='declined'
+                )
+    def decline(self):
+        pass
+
+    @classmethod
+    def create_payment(cls, user, order):
+        new_payment = cls(user)
+        new_payment.order = order
+        new_payment.paymentsystem = paymentsystem
+        new_payment.billAmount = order.order_cost
+        new_payment.save()
+        return new_payment
+
+    class Meta:
+        verbose_name_plural = 'Payments'
+
+    def __str__(self):
+        return 'user {} has payed for order {}'.format(self.user, self.order)
+
+
+class PaymentSystemLog(models.Model):
+    shopOrderNumber = models.ForeignKey('orders.Order',
+                                        on_delete=models.PROTECT, null=True, blank=False
+                                        )
+    payeeId = models.IntegerField(null=True, blank=False)
+    dt = models.DateTimeField(auto_now=True)  # sent_at
+    billAmount = models.FloatField(null=True, blank=False)
+    raw_data = JSONField()
+
+    SHOPBILLID = models.IntegerField(null=True, blank=False)
+    SHOPORDERNUMBER = models.CharField(max_length=50, null=True, blank=False)
+    BILL_AMOUNT = models.FloatField(null=True, blank=False)
+    RESULT = models.SmallIntegerField(null=True, blank=False)
+    raw_response = JSONField()
+
+    def __str__(self):
+        return 'order {} is prossed {}'.format(self.order, self.processed_ok)
